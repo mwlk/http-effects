f1ba61dfd2135cad82dedd228e73f055a983a728
Mirko Wlk <mirkoivowlk@gmail.com>
2022-11-28 01:35:03 -0300

    define routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
+import { UserDetailComponent } from './users/user-detail/user-detail.component';
+import { ListComponent } from './users/list/list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: ListComponent,
+  },
+  {
+    path: 'user/:id',
+    component: UserDetailComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
